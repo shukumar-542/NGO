b2a16b104ddb70d10b9818ac82b1a547391085c8
unknown <shukumar542@gmail.com>
2022-07-27 19:14:04 +0600

    ngo website

---
+++ clientSite/urls.py
@@ -0,0 +1,11 @@
+from turtle import home
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home.as_view(), name='home'),
+    path('about/', views.about.as_view(), name='about')
+    
+
+]
---
+++ clientSite/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+class home(TemplateView):
+      template_name = 'clientSite/home.html'
+
+class about(TemplateView):
+      template_name='clientSite/about.html'
