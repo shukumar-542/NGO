13da361fd0170a50d1183021ebad9647ed88d78a
unknown <shukumar542@gmail.com>
2022-07-31 21:27:32 +0600

    news project sonor-partner done

--- clientSite/urls.py
+++ clientSite/urls.py
@@ -6,7 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.home.as_view(), name='home'),
     path('about/', views.about.as_view(), name='about'),
-    path('donar-partnership', views.donorPartnership.as_view(),name='donor-parntership')
+    path('donar-partnership', views.donorPartnership.as_view(),name='donor-parntership'),
+    path('contact/',views.contact.as_view(),name='contact'),
+    path('project-details/<str:type>', views.Projects.as_view(), name='projects'),
+    path('news/', views.news.as_view(), name='news')
     
 
 ]
--- clientSite/views.py
+++ clientSite/views.py
@@ -10,3 +10,21 @@ class about(TemplateView):
 
 class donorPartnership(TemplateView):
       template_name ='clientSite/donor-partner.html'
+      
+class contact(TemplateView):
+      template_name ='clientSite/contact.html'
+
+class Projects(TemplateView):
+      template_name ='clientSite/projects.html'
+      def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+
+        # for Dynamic url argument value
+            type = kwargs
+            myType = context["type"]
+
+            return context
+
+class news(TemplateView):
+      template_name ='clientSite/news.html'
+
