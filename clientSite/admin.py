4543d39889bb595071d5b35fe686dd2cf0e38a73
unknown <shukumar542@gmail.com>
2022-08-20 11:12:46 +0600

    model done

--- clientSite/admin.py
+++ clientSite/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Founder,Voluntieer,ContactUsMessage
+from .models import Founder,Voluntieer,ContactUsMessage,newsAndEvents,NewsEventImage,Project,Project_image,ImageGallary,GalleryImage,VideoGallery
 
 # Register your models here.
 @admin.register(Founder)
@@ -16,3 +16,33 @@ class VolunteerAdmin(admin.ModelAdmin):
 @admin.register(ContactUsMessage)
 class ContactUsMessageAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ContactUsMessage._meta.get_fields()]
+
+
+@admin.register(newsAndEvents)
+class newsAndEventsAdmin(admin.ModelAdmin):
+    list_display = ['title','description','date','type']
+
+@admin.register(NewsEventImage)
+class NewsEventImageAdmin(admin.ModelAdmin):
+    list_display = ['news_event']
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title','details','start_date','end_date','category']
+
+@admin.register(Project_image)
+class Project_imageAdmin(admin.ModelAdmin):
+    list_display = ['project']
+
+@admin.register(ImageGallary)
+class ImageGallaryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+@admin.register(GalleryImage)
+class GalleryImageAdmin(admin.ModelAdmin):
+    list_display = ['gallery']
+
+@admin.register(VideoGallery)
+class VideoGalleryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
--- clientSite/models.py
+++ clientSite/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
@@ -16,14 +17,17 @@ class Founder(models.Model):
 
 
 class Voluntieer(models.Model):
-      first_name = models.CharField(max_length=70, null=False, blank=False)
-      last_name = models.CharField(max_length=70, null=False, blank=False)
-      email = models.EmailField(max_length=250,null=True,blank=True)
-      phone = models.CharField(max_length=15, null=False, blank=False)
-      about = models.TextField(null=True, blank=True)
-      social_links = models.TextField(null=True, blank=True)
-      image = models.ImageField(upload_to="volunteer/",
-                              default="profile1.png")
+    first_name = models.CharField(max_length=70, null=False, blank=False)
+    last_name = models.CharField(max_length=70, null=False, blank=False)
+    email = models.EmailField(max_length=250,null=True,blank=True)
+    phone = models.CharField(max_length=15, null=False, blank=False)
+    about = models.TextField(null=True, blank=True)
+    social_links = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to="volunteer/",
+                            default="profile1.png")
+
+    def __str__(self):
+        return self.first_name
 
 
 
@@ -33,4 +37,78 @@ class ContactUsMessage(models.Model):
     phone = models.CharField(max_length=15, null=False, blank=False)
     email = models.EmailField(max_length=250, null=True, blank=True)
     message = models.TextField(null=False, blank=False)
+
+
+    def __str__(self):
+        return self.name
+
+
+class newsAndEvents(models.Model):
+    CHOICES = {
+        ('news' ,'News'),
+        ('event','Event') 
+    }
+    title = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    date = models.DateTimeField(null=False, blank=False)
+    type = models.CharField(choices=CHOICES,max_length=20, null=False, blank=False)
+    image = models.ImageField(upload_to='newsEvent/',null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+class NewsEventImage(models.Model):
+    news_event = models.ForeignKey(to=newsAndEvents,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='newsEvent/')
+
+    def __str__(self):
+        return str(self.news_event)
+
+
+class Project(models.Model):
+    CHOICES = {
+        ('donor','Donor'),
+        ('partner','Partner')
+    }
+
+    title = models.CharField(max_length=200, null=False, blank=False)
+    details = models.TextField(null=False, blank=False)
+    start_date = models.DateTimeField(null=False, blank=False)
+    end_date = models.DateTimeField(null=True, blank=True)
+    category = models.CharField(max_length=20,choices=CHOICES, null=False, blank=False)
+    image = models.ImageField(upload_to="projects/",null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Project_image(models.Model):
+    project= models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="projects/")
+
+    def __str__(self):
+        return str(self.project)
+
+class ImageGallary(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    image = models.ImageField(upload_to="gallary/",null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class GalleryImage(models.Model):
+    gallery = models.ForeignKey(to=ImageGallary, on_delete=models.CASCADE)
+    image = models.ImageField(
+        upload_to="gallery/", null=False, blank=False)
+
+    def __str__(self):
+        return str(self.gallery)
+
+class VideoGallery(models.Model):
+    title = models.CharField(max_length=300, null=False, blank=False)
+    video_link = models.URLField(max_length=250, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
 
